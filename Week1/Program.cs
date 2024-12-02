00e30537eae22cc3d07518a5fee95c8969be5969
Christopher Roelle <christopher.roelle@gmail.com>
2024-12-02 15:44:45 -0600

    Initial commit

---
+++ Week1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Week1
+{
+    internal class Program
+    {
+
+        public static void Main() {
+
+            string inputPath = "C:\\Users\\chris\\source\\repos\\AdventOfCode2024\\AdventOfCode2024\\Week1\\input.txt";
+
+            Week1 week1 = new Week1();
+
+            week1.ReadInput(inputPath);
+
+            Console.WriteLine($"Total Distance: {week1.CalculateDistance()}"); // Solution1: 2164381
+            Console.WriteLine($"Total Similarity: {week1.CalculateSimilarityScore()}"); // Solution2: 2164381
+
+        }
+
+
+    }
+}
---
+++ Week1/Week1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventOfCode2024.Week1
+{
+    class Week1
+    {
+        List<int> list1 = new List<int>();
+        List<int> list2 = new List<int>();
+
+        public void ReadInput(string inputPath)
+        {
+            string line;
+
+            try
+            {
+                StreamReader sr = new StreamReader(inputPath);
+
+                //Read the first line
+                line = sr.ReadLine();
+
+                while(line != null)
+                {
+                    //Split the string
+                    string[] values = line.Split("   ");
+
+                    //Left goes to list 1, right to list 2
+                    list1.Add(int.Parse(values[0]));
+                    list2.Add(int.Parse(values[1]));
+
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //Clode the reader
+                sr.Close();
+
+                //Sort the lists
+                list1.Sort();
+                list2.Sort();
+
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+        }
+
+        public int CalculateDistance()
+        {
+            //With the lists sorted, find the difference between the smallest::smallest values.
+            //For each pair (sorted asc), add the difference (absolute).
+
+            int totalDistance = 0;
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                totalDistance += Math.Abs(list1[i] - list2[i]);
+            }
+
+            return totalDistance;
+        }
+
+        public int CalculateSimilarityScore()
+        {
+            //For each entry in the left list, find how many times it occurs in the right list
+            //Multiply the value by its occurance count 
+            //Add all scores together. (even if the number is a dupe in the left list
+
+            int totalSimilarity = 0;
+
+            foreach (int i in list1)
+            {
+                int occurance = 0;
+
+                foreach (int j in list2)
+                {
+                    if (i == j) occurance++;
+                }
+
+                totalSimilarity += i * occurance;
+            }
+
+            return totalSimilarity;
+        }
+
+
+    }
+}
